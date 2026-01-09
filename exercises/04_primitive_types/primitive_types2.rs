3cfb51ba501224d6b07d6a01275a5aa68b4d239f
weibetter <wcc84tennis@gmail.com>
2026-01-08 23:32:50 -0500

    chipping it away

--- exercises/04_primitive_types/primitive_types2.rs
+++ exercises/04_primitive_types/primitive_types2.rs
@@ -15,8 +15,8 @@ fn main() {
     // TODO: Analogous to the example before, declare a variable called `your_character`
     // below with your favorite character.
     // Try a letter, try a digit (in single quotes), try a special character, try a character
-    // from a different language than your own, try an emoji 😉
-    // let your_character = '';
+    // from a different language than your own, try an emoji 😉🦀
+    let your_character = '😉';
 
     if your_character.is_alphabetic() {
         println!("Alphabetical!");
